6bbc1bf12b234f936d3c9977d95a7d4a1615aabb
Julie Hu <jhu21495@gmail.com>
2021-01-25 13:53:00 -0800

    finished mosh beginner tutorial notes

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import java.awt.*;
+import java.util.Scanner;
 // (hello.class) Byte Code --> Java Virtual MAchine --> Native Code
 
 // void means function will not return anything
@@ -11,10 +11,49 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        Point point1 = new Point(x:1,y:1);
-        Point point2 = point1;
-        point1.x = 2;
-        System.out.println(point2);
+        // Point point1 = new Point(x:1,y:1);
+        // Point point2 = point1;
+        // point1.x = 2;
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        // FIZZBUZZ
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("Number: ");
+        // int number = scanner.nextInt();
+
+        // if (number % 3 == 0 && number % 5 == 0) {
+        // System.out.println("Fizzbuzz");
+        // } else if (number % 5 == 0) {
+        // System.out.println("Buzz");
+        // } else if (number % 3 == 0) {
+        // System.out.println("Fizz");
+        // } else
+        // System.out.println(number);
+        // }
+
+        // FOR LOOP
+        // use for loops if you know how many times you need to do something
+        // for (int i = 0; i< 5;i++) {
+        // System.out.println("Hello World");
+        // }
+
+        // WHILE LOOP
+        // use if you do not know how many times
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        // while (!input.equals("quit")) {
+        // System.out.print("Input: ");
+        // input = scanner.next().toLowerCase();
+        // System.out.println(input);
+        // }
+
+        // DO
+        do {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+        } while (!input.equals("quit"));
     }
 }
 
